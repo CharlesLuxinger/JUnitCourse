f82debf41b689164a561be0a64556cb352297713
Charles Luxinger <charlesluxinger@hotmail.com>
2019-07-22 22:23:33 -0300

    3. Teste de remoção de um cliente

--- SistemaBancario/testes/negocio/GerenciadoraClientesTest_Ex2.java
+++ SistemaBancario/testes/negocio/GerenciadoraClientesTest_Ex2.java
@@ -15,43 +15,51 @@ public class GerenciadoraClientesTest_Ex2 {
 	@Test
 	public void testPesquisaCliente() {
 
-		// criando alguns clientes
+		// Criando alguns clientes
 		Cliente cliente01 = new Cliente(1, "Gustavo Farias", 31, "gugafarias@gmail.com", 1, true);
 		Cliente cliente02 = new Cliente(2, "Felipe Augusto", 34, "felipeaugusto@gmail.com", 2, true);
-		
-		// inserindo os clientes criados na lista de clientes do banco
+
+		// Inserindo os clientes criados na lista de clientes do banco
 		List<Cliente> clientesDoBanco = new ArrayList<>();
 		clientesDoBanco.add(cliente01);
 		clientesDoBanco.add(cliente02);
-		
+
+		// Método ao qual desejo testar
 		gerClientes = new GerenciadoraClientes(clientesDoBanco);
-		
+
+		// Retorno do método em teste
 		Cliente cliente = gerClientes.pesquisaCliente(1);
-		
+
+		// Verifica se o ID e o Email são correspondetes
 		assertThat(cliente.getId(), is(1));
-		
+		assertThat(cliente.getEmail(), is("gugafarias@gmail.com"));
+
 	}
-	
+
 	@Test
 	public void testRemoveCliente() {
 
-		// criando alguns clientes
+		// Criando alguns clientes
 		Cliente cliente01 = new Cliente(1, "Gustavo Farias", 31, "gugafarias@gmail.com", 1, true);
 		Cliente cliente02 = new Cliente(2, "Felipe Augusto", 34, "felipeaugusto@gmail.com", 2, true);
-		
-		// inserindo os clientes criados na lista de clientes do banco
+
+		// Inserindo os clientes criados na lista de clientes do banco
 		List<Cliente> clientesDoBanco = new ArrayList<>();
 		clientesDoBanco.add(cliente01);
 		clientesDoBanco.add(cliente02);
-		
+
 		gerClientes = new GerenciadoraClientes(clientesDoBanco);
 		
+		// Retorno e Método ao qual desejo testar
 		boolean clienteRemovido = gerClientes.removeCliente(2);
 		
+		// Verifica o valor da variável
 		assertThat(clienteRemovido, is(true));
+		// Verifica o qtd de clientes
 		assertThat(gerClientes.getClientesDoBanco().size(), is(1));
+		// Verifica o retorno é nulo
 		assertNull(gerClientes.pesquisaCliente(2));
-		
+
 	}
 
 }
