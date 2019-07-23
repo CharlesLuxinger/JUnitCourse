49599e11363a607f86f11b980699b13c513c7fe6
Charles Luxinger <charlesluxinger@hotmail.com>
2019-07-22 22:34:50 -0300

    5. Documentação e comentários

--- SistemaBancario/testes/negocio/GerenciadoraClientesTest_Ex4.java
+++ SistemaBancario/testes/negocio/GerenciadoraClientesTest_Ex4.java
@@ -13,7 +13,7 @@ import org.junit.Test;
  * sobre clientes, realizadas pela classe {@link GerenciadoraClientes}.
  * 
  * @author Gustavo Farias
- * @date 21/01/2035 
+ * @date 21/01/2035
  */
 public class GerenciadoraClientesTest_Ex4 {
 
@@ -29,26 +29,30 @@ public class GerenciadoraClientesTest_Ex4 {
 	public void testPesquisaCliente() {
 
 		/* ========== Montagem do cenário ========== */
-		
+
 		// criando alguns clientes
 		Cliente cliente01 = new Cliente(1, "Gustavo Farias", 31, "gugafarias@gmail.com", 1, true);
 		Cliente cliente02 = new Cliente(2, "Felipe Augusto", 34, "felipeaugusto@gmail.com", 1, true);
-		
+
 		// inserindo os clientes criados na lista de clientes do banco
 		List<Cliente> clientesDoBanco = new ArrayList<>();
 		clientesDoBanco.add(cliente01);
 		clientesDoBanco.add(cliente02);
-		
+
+		// Método ao qual desejo testar
 		gerClientes = new GerenciadoraClientes(clientesDoBanco);
 
 		/* ========== Execução ========== */
+		// Retorno do método em teste
 		Cliente cliente = gerClientes.pesquisaCliente(1);
-		
+
 		/* ========== Verificações ========== */
+		// Verifica se o ID e o Email são correspondetes
 		assertThat(cliente.getId(), is(1));
-		
+		assertThat(cliente.getEmail(), is("gugafarias@gmail.com"));
+
 	}
-	
+
 	/**
 	 * Teste básico da remoção de um cliente a partir do seu ID.
 	 * 
@@ -59,28 +63,30 @@ public class GerenciadoraClientesTest_Ex4 {
 	public void testRemoveCliente() {
 
 		/* ========== Montagem do cenário ========== */
-		
+
 		// criando alguns clientes
 		Cliente cliente01 = new Cliente(1, "Gustavo Farias", 31, "gugafarias@gmail.com", 1, true);
 		Cliente cliente02 = new Cliente(2, "Felipe Augusto", 34, "felipeaugusto@gmail.com", 1, true);
-		
+
 		// inserindo os clientes criados na lista de clientes do banco
 		List<Cliente> clientesDoBanco = new ArrayList<>();
 		clientesDoBanco.add(cliente01);
 		clientesDoBanco.add(cliente02);
-		
-		gerClientes = new GerenciadoraClientes(clientesDoBanco);
-		
-		/* ========== Execução ========== */
-		boolean clienteRemovido = gerClientes.removeCliente(2);
-		
-		/* ========== Verificações ========== */
-		assertThat(clienteRemovido, is(true));
-		assertThat(gerClientes.getClientesDoBanco().size(), is(1));
-		assertNull(gerClientes.pesquisaCliente(2));
-		
-	}
-	
-}
 
-//Documentação e comentários
+		gerClientes = new GerenciadoraClientes(clientesDoBanco);
+
+		/* ========== Execução ========== */
+		// Retorno e Método ao qual desejo testar
+		boolean clienteRemovido = gerClientes.removeCliente(2);
+
+		/* ========== Verificações ========== */
+		// Verifica o valor da variável
+		assertThat(clienteRemovido, is(true));
+		// Verifica o qtd de clientes
+		assertThat(gerClientes.getClientesDoBanco().size(), is(1));
+		// Verifica o retorno é nulo
+		assertNull(gerClientes.pesquisaCliente(2));
+
+	}
+
+}
